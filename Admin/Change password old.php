b545211bd911629d77e4924f440ba7b8421f478b
Surya joshi <jsurya860@gmail.com>
2024-10-24 16:35:06 +0545

    Update Change password old.php

--- Admin/Change password old.php
+++ Admin/Change password old.php
@@ -45,8 +45,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $mail->isSMTP();                                            // Send using SMTP
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
                 $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
-                $mail->Username   = 'jsurya860@gmail.com';                  // SMTP username
-                $mail->Password   = 'vgmwvfxetkvysbgv';                      // SMTP password
+                $mail->Username   = '********@gmail.com';                  // SMTP username
+                $mail->Password   = '**********';                      // SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
                 $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         
